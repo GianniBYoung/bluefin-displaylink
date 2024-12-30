117fcf0a60e0d7e3186e42b2fc7a0994c9486f5d
Young, Gianni <cmancy@tutanota.com>
2024-12-30 13:39:04 -0600

    Revert "piper for logitech configuration"
    This reverts commit 8e8f4fb9091898c04bd9a90992e5512c94aadcaf.

--- build.sh
+++ build.sh
@@ -25,6 +25,7 @@ rpm-ostree install screen
 rpm-ostree install kitty zsh
 rpm-ostree install kitty zsh ghostty
 rpm-ostree install kitty zsh ghostty piper
+rpm-ostree install kitty zsh ghostty
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
